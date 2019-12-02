cce29c6002a71d37baaaac8ce7c72d89d553b93f
tarpaha <ppopchikovsky@gmail.com>
2019-12-02 14:49:42 +0300

    day-2019-12-02

---
+++ Day-2019-12-02.App/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day_2019_12_02.App
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var input = File
+                .ReadAllText("data/input.txt")
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            
+            SolvePart1(input);
+            SolvePart2(input);
+        }
+
+        private static void SolvePart1(int[] input)
+        {
+            var program = new int[input.Length];
+            Array.Copy(input, program, input.Length);
+            program[1] = 12;
+            program[2] = 2;
+            Computer.Compute(program);
+            Console.WriteLine($"Part1: {program[0]}");
+        }
+
+        private static void SolvePart2(int[] input)
+        {
+            var program = new int[input.Length];
+            for (var noun = 0; noun < 100; noun++)
+            {
+                for (var verb = 0; verb < 100; verb++)
+                {
+                    Array.Copy(input, program, input.Length);
+                    program[1] = noun;
+                    program[2] = verb;
+                    Computer.Compute(program);
+                    if (program[0] == 19690720)
+                    {
+                        Console.WriteLine($"Part2: {100 * noun + verb}");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Day-2019-12-02/Computer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Day_2019_12_02
+{
+    public static class Computer
+    {
+        public static void Compute(int[] program)
+        {
+            var ip = 0;
+            while (true)
+            {
+                switch (program[ip])
+                {
+                    case 1:
+                    {
+                        var parameter1 = program[ip + 1];
+                        var parameter2 = program[ip + 2];
+                        var parameter3 = program[ip + 3];
+                        program[parameter3] = program[parameter1] + program[parameter2];
+                        ip += 4;
+                        break;
+                    }
+                    case 2:
+                    {
+                        var parameter1 = program[ip + 1];
+                        var parameter2 = program[ip + 2];
+                        var parameter3 = program[ip + 3];
+                        program[parameter3] = program[parameter1] * program[parameter2];
+                        ip += 4;
+                        break;
+                    }
+                    case 99:
+                        return;
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
