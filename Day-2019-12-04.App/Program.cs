3f935584be38eadc55185c1cc6b41fd01bb15968
tarpaha <ppopchikovsky@gmail.com>
2019-12-04 17:51:29 +0300

    one string solution added (commented)

--- Day-2019-12-04.App/Program.cs
+++ Day-2019-12-04.App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using System.Linq;
 
 namespace Day_2019_12_04.App
 {
@@ -14,12 +15,30 @@ namespace Day_2019_12_04.App
 
         private static void SolvePart1(int min, int max)
         {
+            /* One string solution
+            var correctPasswordsCount = Enumerable
+                .Range(min, max - min + 1)
+                .AsParallel()
+                .Select(n => n.ToString())
+                .Where(s => s.SequenceEqual(s.OrderBy(ch => ch)))
+                .Where(s => s.GroupBy(ch => ch).Any(g => g.Count() > 1))
+                .Count(); */
+
             var correctPasswordsCount = Solver.GetCorrectPasswordsFromRangeType(min, max, Password.IsCorrectType1);
             Console.WriteLine($"Part1: {correctPasswordsCount}");
         }
         
         private static void SolvePart2(int min, int max)
         {
+            /* One string solution
+            var correctPasswordsCount = Enumerable
+                .Range(min, max - min + 1)
+                .AsParallel()
+                .Select(n => n.ToString())
+                .Where(s => s.SequenceEqual(s.OrderBy(ch => ch)))
+                .Where(s => s.GroupBy(ch => ch).Any(g => g.Count() == 2))
+                .Count(); */
+
             var correctPasswordsCount = Solver.GetCorrectPasswordsFromRangeType(min, max, Password.IsCorrectType2);
             Console.WriteLine($"Part2: {correctPasswordsCount}");
         }
