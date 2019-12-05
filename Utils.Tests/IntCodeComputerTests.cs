bc1807f47f926b52eb7ba29f423e1b64417d6290
tarpaha <ppopchikovsky@gmail.com>
2019-12-05 18:19:08 +0300

    input and output tests were merged with other execution tests

--- Utils.Tests/IntCodeComputerTests.cs
+++ Utils.Tests/IntCodeComputerTests.cs
@@ -47,47 +47,29 @@ namespace Utils.Tests
             return Enumerable.Repeat(0, length).Select(_ => _random.Next()).ToArray();
         }
 
-        [TestCase("5", "3,0,99", "5,0,99")]
-        [TestCase("12,34", "3,5,3,6,99,0,0", "3,5,3,6,99,12,34")]
-        public void TestInput(string inputStr, string programStr, string memoryStr)
+        [TestCase("", "1,0,0,0,99", "2,0,0,0,99", "")]
+        [TestCase("", "2,3,0,3,99", "2,3,0,6,99", "")]
+        [TestCase("", "2,4,4,5,99,0", "2,4,4,5,99,9801", "")]
+        [TestCase("", "1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99", "")]
+        [TestCase("5", "3,0,99", "5,0,99", "")]
+        [TestCase("12,34", "3,5,3,6,99,0,0", "3,5,3,6,99,12,34", "")]
+        [TestCase("", "4,0,99", "4,0,99", "4")]
+        [TestCase("", "4,9,4,8,4,7,99,1,2,3", "4,9,4,8,4,7,99,1,2,3", "3,2,1")]
+        public void TestExecution(string inputStr, string programStr, string memoryStr, string outputStr)
         {
-            _computer.LoadProgram(Parse(programStr));
-            _computer.SetInput(Parse(inputStr));
-            _computer.Execute();
-            Assert.That(_computer.GetMemory(), Is.EquivalentTo(Parse(memoryStr)));
-        }
-
-        [TestCase("4,0,99", "4")]
-        [TestCase("4,9,4,8,4,7,99,1,2,3", "3,2,1")]
-        public void TestOutput(string programStr, string outputStr)
-        {
-            _computer.LoadProgram(Parse(programStr));
-            _computer.Execute();
-            Assert.That(_computer.GetOutput(), Is.EquivalentTo(Parse(outputStr)));
-            _computer.LoadProgram(Parse("1,0,0,0,99"));
-            _computer.Execute();
-            Assert.That(_computer.GetOutput(), Is.Empty);
-        }
-        
-        [TestCase("1,0,0,0,99", "2,0,0,0,99")]
-        [TestCase("2,3,0,3,99", "2,3,0,6,99")]
-        [TestCase("2,4,4,5,99,0", "2,4,4,5,99,9801")]
-        [TestCase("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
-        public void TestExecution(string programStr, string resultStr)
-        {
-            var program = Parse(programStr);
-            var result = Parse(resultStr);
-            
             var computer = new IntCodeComputer();
-            computer.LoadProgram(program);
+            computer.LoadProgram(Parse(programStr));
+            computer.SetInput(Parse(inputStr));
             computer.Execute();
-
-            Assert.That(computer.GetMemory(), Is.EquivalentTo(result));
+            Assert.That(computer.GetMemory(), Is.EquivalentTo(Parse(memoryStr)));
+            Assert.That(computer.GetOutput(), Is.EquivalentTo(Parse(outputStr)));
         }
 
-        private static int[] Parse(string program)
+        private static int[] Parse(string str)
         {
-            return program.Split(',').Select(int.Parse).ToArray();
+            return string.IsNullOrEmpty(str)
+                ? new int[0]
+                : str.Split(',').Select(int.Parse).ToArray();
         }
     }
 }
