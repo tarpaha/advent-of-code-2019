4e44701d6193067d38ce56731d719b75caf2858f
tarpaha <ppopchikovsky@gmail.com>
2019-12-08 20:38:10 +0300

    solution test for day 5

---
+++ Day-2019-12-05.App/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Utils;
+
+namespace Day_2019_12_05.App
+{
+    public class Solution : ISolution
+    {
+        public static void Main()
+        {
+            var solution = new Solution();
+            Console.WriteLine($"Part1: {solution.SolvePart1()}");
+            Console.WriteLine($"Part2: {solution.SolvePart2()}");
+        }
+        
+        private readonly Computer _computer;
+
+        public Solution()
+        {
+            var program = Input.GetData()
+                .Split(',')
+                .Select(int.Parse)
+                .ToArray();
+            
+            _computer = new Computer();
+            _computer.LoadProgram(program);
+        }
+
+        public int SolvePart1()
+        {
+            _computer.RestoreMemory();
+            _computer.SetInput(new[] {1});
+            _computer.Execute();
+            return _computer.GetOutput().Last();
+        }
+
+        public int SolvePart2()
+        {
+            _computer.RestoreMemory();
+            _computer.SetInput(new[] {5});
+            _computer.Execute();
+            return _computer.GetOutput().Last();
+        }
+    }
+}
--- Solutions.Tests/Tests.cs
+++ Solutions.Tests/Tests.cs
@@ -14,6 +14,7 @@ namespace Solutions.Tests
                 yield return new TestCaseData(new Day_2019_12_02.App.Solution(), 3790689, 6533);
                 yield return new TestCaseData(new Day_2019_12_03.App.Solution(), 627, 13190);
                 yield return new TestCaseData(new Day_2019_12_04.App.Solution(), 2150, 1462);
+                yield return new TestCaseData(new Day_2019_12_05.App.Solution(), 15097178, 1558663);
             }
         }
 
