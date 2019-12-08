7e02dda61983e3ea60c83483364b63bfe6be4494
tarpaha <ppopchikovsky@gmail.com>
2019-12-08 18:44:36 +0300

    solution test for day 3

---
+++ Day-2019-12-03.App/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Utils;
+
+namespace Day_2019_12_03.App
+{
+    public class Solution : ISolution
+    {
+        public static void Main()
+        {
+            var solution = new Solution();
+            Console.WriteLine($"Part1: {solution.SolvePart1()}");
+            Console.WriteLine($"Part2: {solution.SolvePart2()}");
+        }
+
+        private readonly IEnumerable<Segment2> _wire0;
+        private readonly IEnumerable<Segment2> _wire1;
+
+        public Solution()
+        {
+            var steps = Input.GetData()
+                .Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+            _wire0 = StepsParser.Parse(steps[0]);
+            _wire1 = StepsParser.Parse(steps[1]);
+        }
+
+        public int SolvePart1()
+        {
+            return IntersectionFinder.FindDistanceOfClosestToOrigin(_wire0, _wire1);
+        }
+
+        public int SolvePart2()
+        {
+            return SignalMinimizer.FindMinDistanceToIntersection(_wire0, _wire1);
+        }
+    }
+}
