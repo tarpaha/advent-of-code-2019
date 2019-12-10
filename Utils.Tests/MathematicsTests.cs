4ee06c12fba6ba0ec05712fe3ff9f074d953e2b0
tarpaha <ppopchikovsky@gmail.com>
2019-12-10 13:15:40 +0300

    reduce fraction fix for zero values

--- Utils.Tests/MathematicsTests.cs
+++ Utils.Tests/MathematicsTests.cs
@@ -9,8 +9,8 @@ namespace Utils.Tests
         [TestCase(-9, -3, -3, -1, 3)]
         [TestCase(-12, 20, -3, 5, 4)]
         [TestCase(20, -5, 4, -1, 5)]
-        [TestCase(0, 10, 0, 10, 1)]
-        [TestCase(-10, 0, -10, 0, 1)]
+        [TestCase(0, 10, 0, 1, 10)]
+        [TestCase(-10, 0, -1, 0, 10)]
         public void ReduceFractionTests(int a1, int b1, int a2, int b2, int n)
         {
             Assert.That(Mathematics.ReduceFraction(a1, b1), Is.EqualTo((a2, b2, n)));
--- Utils/Mathematics.cs
+++ Utils/Mathematics.cs
@@ -6,6 +6,11 @@ namespace Utils
     {
         public static (int a, int b, int n) ReduceFraction(int a, int b)
         {
+            if (a == 0)
+                return (0, b > 0 ? 1 : -1, Math.Abs(b));
+            if (b == 0)
+                return (a > 0 ? 1 : -1, 0, Math.Abs(a));
+            
             var n = 1;
             var min = Math.Abs(a) < Math.Abs(b) ? Math.Abs(a) : Math.Abs(b);
             for (var div = 2; div <= min; div++)
