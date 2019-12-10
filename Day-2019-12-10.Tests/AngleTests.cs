24a9f8edbca1f2afec88d1b6b3e152867dd07c6d
tarpaha <ppopchikovsky@gmail.com>
2019-12-10 19:41:08 +0300

    angle calculations with tests

---
+++ Day-2019-12-10.Tests/AngleTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Day_2019_12_10.Tests
+{
+    public class AngleTests
+    {
+        [TestCase(".B.|.A.|...", 0.0f)]
+        [TestCase("..B|.A.|...", 45.0f)]
+        [TestCase("...|.AB|...", 90.0f)]
+        [TestCase("...|.A.|..B", 135.0f)]
+        [TestCase("...|.A.|.B.", 180.0f)]
+        [TestCase("...|.A.|B..", 225.0f)]
+        [TestCase("...|BA.|...", 270.0f)]
+        [TestCase("B..|.A.|...", 315.0f)]
+        public void Test(string data, float angle)
+        {
+            var planets = Reader.Read(data).ToList();
+            var planetA = planets.First(planet => planet.Name == "A");
+            var planetB = planets.First(planet => planet.Name == "B");
+            Assert.That(Angle.Calculate(planetA, planetB), Is.EqualTo(angle));
+        }
+    }
+}
---
+++ Day-2019-12-10/Angle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day_2019_12_10
+{
+    public static class Angle
+    {
+        // angle from source-up to source-target, clockwise 0...360
+        public static float Calculate(IAsteroid source, IAsteroid target)
+        {
+            var vx = target.X - source.X;
+            var vy = target.Y - source.Y;
+            var angle = Math.Atan2(vx, -vy) * 180.0 / Math.PI;
+            return (float) (angle < 0 ? angle + 360 : angle);
+        }
+    }
+}
