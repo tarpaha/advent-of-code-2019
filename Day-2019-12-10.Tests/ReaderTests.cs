6223133fd7db240e80fc1f5e43a46926c1f61d36
tarpaha <ppopchikovsky@gmail.com>
2019-12-10 12:17:13 +0300

    more complex reader test

--- Day-2019-12-10.Tests/ReaderTests.cs
+++ Day-2019-12-10.Tests/ReaderTests.cs
@@ -5,15 +5,17 @@ namespace Day_2019_12_10.Tests
 {
     public class ReaderTests
     {
-        [TestCase("..#|...", "#", 2, 0)]
-        public void Test(string data, string name, int x, int y)
-        {
-            var planets = Reader.Read(data).ToList();
-            Assert.That(planets.Count, Is.EqualTo(1));
-            var planet = planets.First();
-            Assert.That(planet.Name, Is.EqualTo(name));
-            Assert.That(planet.X, Is.EqualTo(x));
-            Assert.That(planet.Y, Is.EqualTo(y));
+        [TestCase("..A|B.C", new [] { "A", "B", "C" }, new [] { 2, 0, 0, 1, 2, 1 })]
+        public void Test(string data, string[] names, int[] coords)
+        { 
+            var planets = Reader.Read(data).ToArray();
+            Assert.That(planets.Length, Is.EqualTo(names.Length));
+            for (var i = 0; i < planets.Length; i++)
+            {
+                Assert.That(planets[i].Name, Is.EqualTo(names[i]));
+                Assert.That(planets[i].X, Is.EqualTo(coords[2 * i]));
+                Assert.That(planets[i].Y, Is.EqualTo(coords[2 * i + 1]));
+            }
         }
     }
 }
