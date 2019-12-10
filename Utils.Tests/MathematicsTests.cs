2fedea6588b8c883b8492662fd833bdd81ef2a05
tarpaha <ppopchikovsky@gmail.com>
2019-12-10 12:53:07 +0300

    mathematics class and reduce fraction function

---
+++ Utils.Tests/MathematicsTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Utils.Tests
+{
+    public class MathematicsTests
+    {
+        [TestCase(1, 2, 1, 2, 1)]
+        [TestCase(2, 4, 1, 2, 2)]
+        [TestCase(-9, -3, -3, -1, 3)]
+        [TestCase(-12, 20, -3, 5, 4)]
+        [TestCase(20, -5, 4, -1, 5)]
+        [TestCase(0, 10, 0, 10, 1)]
+        [TestCase(-10, 0, -10, 0, 1)]
+        public void ReduceFractionTests(int a1, int b1, int a2, int b2, int n)
+        {
+            Assert.That(Mathematics.ReduceFraction(a1, b1), Is.EqualTo((a2, b2, n)));
+        }
+    }
+}
---
+++ Utils/Mathematics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Utils
+{
+    public static class Mathematics
+    {
+        public static (int a, int b, int n) ReduceFraction(int a, int b)
+        {
+            var n = 1;
+            var min = Math.Abs(a) < Math.Abs(b) ? Math.Abs(a) : Math.Abs(b);
+            for (var div = 2; div <= min; div++)
+            {
+                if (a % div == 0 && b % div == 0)
+                {
+                    n *= div;
+                    a /= div;
+                    b /= div;
+                    min /= div;
+                    div = 1;
+                }
+            }
+            return (a, b, n);
+        }
+    }
+}
