f4e025ddcb71386d3e80345bd7e54fd1d51c37a9
tarpaha <ppopchikovsky@gmail.com>
2019-12-10 12:18:08 +0300

    planet interface was extracted to separate file

---
+++ Day-2019-12-10/IPlanet.cs
@@ -0,0 +1,9 @@
+﻿namespace Day_2019_12_10
+{
+    public interface IPlanet
+    {
+        int X { get; }
+        int Y { get; }
+        string Name { get; }
+    }
+}
--- Day-2019-12-10/Planet.cs
+++ Day-2019-12-10/Planet.cs
@@ -1,12 +1,5 @@
 ﻿namespace Day_2019_12_10
 {
-    public interface IPlanet
-    {
-        int X { get; }
-        int Y { get; }
-        string Name { get; }
-    }
-
     public class Planet : IPlanet
        {
         public int X { get; }
