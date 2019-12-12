b65666df090b835aebc3249b116ad509d53f19b6
tarpaha <ppopchikovsky@gmail.com>
2019-12-12 12:13:49 +0300

    vector 3 struct for day 12

---
+++ Utils.Tests/Vector3Tests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Utils.Tests
+{
+    public class Vector3Tests
+    {
+        private static IEnumerable<TestCaseData> OperatorPlusTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new Vector3(1, 2, 3), new Vector3(-3, -2, -1), new Vector3(-2, 0, 2));
+            }
+        }
+
+        [TestCaseSource("OperatorPlusTestCases")]
+        public void Test(Vector3 a, Vector3 b, Vector3 c)
+        {
+            Assert.That(a + b, Is.EqualTo(c));
+        }
+    }
+}
---
+++ Utils/Vector3.cs
@@ -0,0 +1,25 @@
+﻿namespace Utils
+{
+    public struct Vector3
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Vector3(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public override string ToString()
+        {
+            return $"({x}, {y}, {z})";
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+    }
+}
