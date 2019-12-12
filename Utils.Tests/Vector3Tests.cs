a44b820db306a914c30d47dac96ea576edd58e28
tarpaha <ppopchikovsky@gmail.com>
2019-12-12 12:50:10 +0300

    more vector3 operators with tests

--- Utils.Tests/Vector3Tests.cs
+++ Utils.Tests/Vector3Tests.cs
@@ -5,18 +5,51 @@ namespace Utils.Tests
 {
     public class Vector3Tests
     {
+        private static IEnumerable<TestCaseData> OperatorUnaryMinusTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+                yield return new TestCaseData(new Vector3(1, 2, 3), new Vector3(-1, -2, -3));
+                yield return new TestCaseData(new Vector3(-4, -5, -6), new Vector3(4, 5, 6));
+            }
+        }
+        
         private static IEnumerable<TestCaseData> OperatorPlusTestCases
         {
             get
             {
+                yield return new TestCaseData(new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
                 yield return new TestCaseData(new Vector3(1, 2, 3), new Vector3(-3, -2, -1), new Vector3(-2, 0, 2));
             }
         }
+        
+        private static IEnumerable<TestCaseData> OperatorMinusTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new Vector3(0, 0, 0), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+                yield return new TestCaseData(new Vector3(1, 2, 3), new Vector3(-3, -2, -1), new Vector3(4, 4, 4));
+            }
+        }
 
+        [TestCaseSource("OperatorUnaryMinusTestCases")]
+        public void UnaryMinusTests(Vector3 a, Vector3 b)
+        {
+            Assert.That(-a, Is.EqualTo(b));
+        }
+        
+        
         [TestCaseSource("OperatorPlusTestCases")]
-        public void Test(Vector3 a, Vector3 b, Vector3 c)
+        public void PlusTests(Vector3 a, Vector3 b, Vector3 c)
         {
             Assert.That(a + b, Is.EqualTo(c));
+        }
+        
+        [TestCaseSource("OperatorMinusTestCases")]
+        public void MinusTests(Vector3 a, Vector3 b, Vector3 c)
+        {
+            Assert.That(a - b, Is.EqualTo(c));
         }
     }
 }
