e7d0f458e4b07d03127f61b07c261925aaeeeeb9
tarpaha <ppopchikovsky@gmail.com>
2019-12-20 19:47:48 +0300

    day-2019-12-16 structure, pattern generation with tests

---
+++ Day-2019-12-16.Tests/PatternTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Day_2019_12_16.Tests
+{
+    public class PatternTests
+    {
+        [TestCase(4, 1, "1,0,-1,0")]
+        [TestCase(5, 1, "1,0,-1,0,1")]
+        [TestCase(15, 2, "0,1,1,0,0,-1,-1,0,0,1,1,0,0,-1,-1")]
+        public void Test(int length, int position, string patternStr)
+        {
+            var pattern = Pattern.Generate(length, position);
+            Assert.That(string.Join(",", pattern), Is.EqualTo(patternStr));
+        }
+    }
+}
---
+++ Day-2019-12-16/Pattern.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Day_2019_12_16
+{
+    public static class Pattern
+    {
+        private static readonly int[] BasePattern = {0, 1, 0, -1};
+
+        public static int[] Generate(int length, int position)
+        {
+            var pattern = new List<int>(length + 1);
+            var basePatternPosition = 0;
+            var repeatCount = position;
+            for (var patternPosition = 0; patternPosition <= length; patternPosition++)
+            {
+                pattern.Add(BasePattern[basePatternPosition]);
+                if (--repeatCount == 0)
+                {
+                    if (++basePatternPosition == BasePattern.Length)
+                        basePatternPosition = 0;
+                    repeatCount = position;
+                }
+            }
+            pattern.RemoveAt(0);
+            return pattern.ToArray();
+        }
+    }
+}
